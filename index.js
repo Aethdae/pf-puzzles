95f0b59ec518ec4a1427346a68ed3c63e3e33a5c
Tyler Long <aethdae@gmail.com>
2026-03-14 00:37:29 -0500

    add images, change div/img classes

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function main() {
     runes[i].addEventListener("click", () => {
       if (runes[i].dataset.isChecked == "false") {
         runes[i].dataset.isChecked = "true";
+        //runes[i].classList.add("shadow-[0 4px 6px]", "shadow-white");
       } else {
         runes[i].dataset.isChecked = "false";
+        //runes[i].classList.remove("shadow-2xl", "shadow-white");
       }
     });
   }
